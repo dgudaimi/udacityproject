a890ee9bccedd8a4b377415eab738a2b0f1937aa
Daniah Algudaimi <dgudaimi@gmail.com>
2021-01-19 19:10:10 +0300

    Changes to be committed: 	modified:   README.md, added line to file 	modified:   bikeshare.py, added part of a comment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # udacityproject
 This is an edit for Section 2 Task C
+
+This is an editional edit for section 3 task B. 
--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day to analyze. This is an editional edit for the github project, section 3 task B
 
     Returns:
         (str) city - name of the city to analyze
